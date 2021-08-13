b74dbce73490309dca8f18a2fc7c8e47dbfbd1e6
Weston Dransfield <weston@Westons-MacBook-Pro.local>
2021-08-13 15:04:57 -0600

    Add course and event models

---
+++ Study Helper/Models/Course.swift
@@ -0,0 +1,17 @@
+//
+//  Course.swift
+//  Study Helper
+//
+//  Created by Weston Dransfield on 8/13/21.
+//
+
+import Foundation
+
+struct Course {
+    let id: String
+    let name: String
+    let courseColor: String
+    let courseImage: URL?
+    let totalSeconds: Double
+}
+
---
+++ Study Helper/Models/Event.swift
@@ -0,0 +1,15 @@
+//
+//  Event.swift
+//  Study Helper
+//
+//  Created by Weston Dransfield on 8/13/21.
+//
+
+import Foundation
+
+struct Event {
+    let id: String
+    let course: Course
+    let startTime: Date
+    let endTime: Date?
+}
