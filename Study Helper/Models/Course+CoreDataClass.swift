74a4c200cb596f9bc3a6142109fc5e6c52676c8b
westonkd <wdrans@gmail.com>
2021-08-14 20:14:21 -0600

    Replace struct models with core data classes

---
+++ Study Helper/Models/Course+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Course+CoreDataClass.swift
+//  Study Helper
+//
+//  Created by Weston Dransfield on 8/14/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Course)
+public class Course: NSManagedObject {
+
+}
---
+++ Study Helper/Models/Event+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Event+CoreDataClass.swift
+//  Study Helper
+//
+//  Created by Weston Dransfield on 8/14/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Event)
+public class Event: NSManagedObject {
+
+}
