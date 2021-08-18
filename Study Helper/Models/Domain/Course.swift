fc9e90b1e013c29316a76db8866822edf8df1657
westonkd <wdrans@gmail.com>
2021-08-18 12:09:24 -0600

    fix indentation in Course.swift

--- Study Helper/Models/Domain/Course.swift
+++ Study Helper/Models/Domain/Course.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 struct Course: DomainModel {
-  typealias Model = Course
-
-  let id: String
-  let name: String
-  let courseColor: String
-  let courseImage: URL?
-  let totalSeconds: Double
-   
+    typealias Model = Course
+    
+    let id: String
+    let name: String
+    let courseColor: String
+    let courseImage: URL?
+    let totalSeconds: Double
+    
     static func all() -> [Course] {
         let courseStore = CDCourseStore(context: PersistenceController.shared.container.viewContext)
         let result = courseStore.all()
@@ -30,18 +30,18 @@ struct Course: DomainModel {
         return []
     }
     
-  func save() -> Bool {
-    let courseStore = CDCourseStore(context: PersistenceController.shared.container.viewContext)
-    
-    // TODO: if the course exists, just update it
-    let result = courseStore.create(course: self)
-    
-    switch result {
-    case .success:
-        courseStore.save()
-        return true
-    case .failure:
-        return false
+    func save() -> Bool {
+        let courseStore = CDCourseStore(context: PersistenceController.shared.container.viewContext)
+        
+        // TODO: if the course exists, just update it
+        let result = courseStore.create(course: self)
+        
+        switch result {
+        case .success:
+            courseStore.save()
+            return true
+        case .failure:
+            return false
+        }
     }
-  }
 }
