9dd0ba9d6c3310467e91939881aa4f8d1743b3e8
westonkd <wdrans@gmail.com>
2021-09-06 21:40:05 -0600

    Make course grid feel better
    This change does the following:
    - Makes the active course pulse
      to indicate a timer is currently
      running.
    - On finger down, animate a small scale
      up effect
    - on finger up, animate a small scale
      down effect

--- Study Helper/Views/CourseButton.swift
+++ Study Helper/Views/CourseButton.swift
@@ -8,38 +8,74 @@
 import SwiftUI
 
 struct CourseButton: View {
+    enum Shadow: CGFloat {
+        case none = 0
+        case small = 5
+        case large = 10
+    }
+    
     @Binding var course: Course
-
+    @State var shadowRadius = Shadow.none
+    @State var showBorder = false
+    @State var scaleValue = CGFloat(1)
+    
     var isActiveCourse: Bool
     var onPress: (_ c: Course) -> Void
     
     var body: some View {
-        Button(action: { print("start/stop timer") }) {
-            HStack(alignment: .top) {
-                VStack(alignment: .leading) {
-                    Text(course.name)
-                        .font(.headline)
-                    Text("00:12:20")
-                        .font(.subheadline)
-                        .padding(.top, 5)
+        ZStack {
+            Button(action: {
+                print("start/stop timer")
+            }) {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading) {
+                        Text(course.name)
+                            .font(.headline)
+                        Text("00:12:20")
+                            .font(.subheadline)
+                            .padding(.top, 5)
+                    }
+                    Spacer()
+                    Button(action: {
+                        onPress(course)
+                    }) {
+                        Image(systemName: "ellipsis.circle.fill")
+                            .font(.system(size: 20.0))
+                    }
                 }
-                Spacer()
-                Button(action: { onPress(course) }) {
-                    Image(systemName: "ellipsis.circle.fill")
-                        .font(.system(size: 20.0))
+                .gesture(DragGesture(minimumDistance: 0.0)
+                            .onChanged { _ in withAnimation(.easeOut(duration: 0.2)) { scaleValue = 1.05 } }
+                            .onEnded { _ in withAnimation(.easeIn(duration: 0.2)) {scaleValue = 1} })
+            }
+            .padding(10)
+            .frame(maxWidth: .infinity)
+            .frame(height: 100)
+            .foregroundColor(.white)
+            .background(Color.init(hex: course.courseColor))
+            .cornerRadius(10)
+            .overlay(
+                Group {
+                    if showBorder {
+                        RoundedRectangle(cornerRadius: 10).stroke(Color.init(hex: course.courseColor).opacity(0.75), lineWidth: 4)
+                    }
+                }
+            )
+            .scaleEffect(scaleValue)
+            .shadow(radius: shadowRadius.rawValue)
+            .onAppear {
+                if (!isActiveCourse) {
+                    return
+                }
+                
+                shadowRadius = Shadow.small
+                
+                let anim = Animation.easeInOut(duration: 0.6).repeatForever(autoreverses: true)
+                withAnimation(anim) {
+                    shadowRadius = Shadow.large
+                    showBorder = true
                 }
             }
         }
-        .padding(10)
-        .frame(maxWidth: .infinity)
-        .frame(height: 100)
-        .foregroundColor(.white)
-        .background(Color.init(hex: course.courseColor))
-        .cornerRadius(10)
-        .overlay(
-            isActiveCourse ? RoundedRectangle(cornerRadius: 10)
-                .stroke(Color(.label), lineWidth: 3) : nil
-        )
     }
 }
 
